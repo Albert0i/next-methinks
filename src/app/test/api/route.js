54f007393d510e264ac286f277e01584a0161991
Alberto Iong <albert0i@hotmail.com>
2023-08-04 11:52:32 +0800

    add test folder

---
+++ src/app/test/api/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req, { params }) {
+  const searchParams = req.nextUrl.searchParams  
+
+  console.log('params=', params)
+  console.log('searchParams=', searchParams)
+
+  return NextResponse.json({success: true}, { status: 200 }) 
+}
+
+/*
+   http://localhost:3000/test/api?bldcod=LK&blocod=01
+*/
---
+++ src/app/test/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const TestPage = ( props ) => {  
+  console.log('props=', props)
+  const { params, searchParams } = props   
+
+  return (
+    <>
+        <div>TestPage</div>
+        <p>params={JSON.stringify(params)}</p>
+        <p>searchParams={JSON.stringify(searchParams)}</p>
+    </>    
+  )
+}
+
+export default TestPage
+
+/*
+   http://localhost:3000/test?bldcod=LK&blocod=01
+*/
