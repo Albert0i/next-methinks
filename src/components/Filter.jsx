d14fbb1420a08c29059f2843e84f5fe60a50da35
Alberto Iong <albert0i@hotmail.com>
2023-08-10 12:23:39 +0800

    add some bottom margin to Filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -18,7 +18,7 @@ const Filter = () => {
   }
 
   return (
-    <div className='flex flex-row justify-end px-2'>
+    <div className='flex flex-row justify-end px-2 mb-1'>
         <input className='w-1/4 px-1 border border-black rounded' placeholder='full text value' autoFocus type='text' value={filterValue} onChange={ e => setFilterValue(e.target.value)}></input>
         <button className='px-1 ml-2 font-bold text-white bg-green-500 rounded' onClick={ handleClick }>Filter</button>
         { showLogout ? <Link className='px-1 ml-2 font-bold text-white bg-orange-500 rounded' href='/posts/logout'>Logout</Link> : ''}
