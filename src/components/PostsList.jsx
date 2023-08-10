4d7ac5070d2bac673938731aba336845847800b5
Alberto Iong <albert0i@hotmail.com>
2023-08-10 10:00:26 +0800

    show no record when empty

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,6 +5,11 @@ import PostPreview from "./PostPreview";
 export default async function PostsList({searchText}) {  
   const { posts } = await getAllPosts(searchText);
 
+  if (posts.length===0)
+  return (
+          <h1 className='mt-20 text-2xl text-center'>No posts found</h1>    
+    )
+
   return (
     <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3">
       { posts.map(post => <PostPreview key={post._id} {...post} />  ) }
