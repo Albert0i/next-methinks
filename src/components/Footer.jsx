695ce87eb15792bd52ca4d4509d55decaad667f1
Alberto Iong <albert0i@hotmail.com>
2023-08-10 12:13:35 +0800

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="py-2 mt-2 text-center text-slate-400">
-        <h3>&copy; 2023. Designed by Pixegami</h3>
+        <h3>&copy; 2023. Running on { process.env.APP_ENV }</h3>
       </div>
     </footer>
   )
