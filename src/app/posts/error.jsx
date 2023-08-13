cb2accb5bfe3ace7674ceaea95650e68798df3cb
albert0i <albert0i@hotmail.com>
2023-08-13 09:52:23 +0800

    add error and loading page to posts route

---
+++ src/app/posts/error.jsx
@@ -0,0 +1,11 @@
+'use client'
+ 
+export default function Error({ error, reset }) {
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='mt-20 mb-6 text-2xl text-center'>Something went wrong!</h1>
+      
+      <button className='px-4 py-2 ml-2 font-bold text-white bg-green-500 rounded w-fit' onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,7 @@
+ export default function Loading() {
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='mt-20 mb-6 text-2xl text-center'>Please wait while loading...</h1>
+    </div>
+  )
+}
