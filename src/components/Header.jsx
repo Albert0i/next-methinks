b2af6443367fa12702c988395f58cbb836e1418a
Alberto Iong <albert0i@hotmail.com>
2023-08-14 11:42:39 +0800

    add finishing touch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className='h-[66px]'>
-      <div className="p-2 my-2 text-center rounded-md bg-slate-800 flex flex-row justify-between">
+      <div className="flex flex-row justify-between p-2 my-2 text-center rounded-md bg-slate-800">
 
         <Link href='/posts/create'>
-          <div className="hover:bg-slate-600 mt-2 text-2xl font-bold text-white border border-white rounded px-2">+</div>
+          <div className="px-2 mt-2 text-2xl font-bold text-white border border-white rounded hover:bg-slate-600">+</div>
         </Link>
 
         <Link href="/posts">
-          <div className="hover:bg-slate-600 mt-2 text-2xl font-bold text-white">All Posts</div>
+          <div className="mt-2 text-2xl font-bold text-white hover:rounded hover:bg-slate-600">All Posts</div>
         </Link>
 
         <Link href='/posts/create'>
-          <div className="hover:bg-slate-600 mt-2 text-2xl font-bold text-white border border-white rounded px-2">+</div>
+          <div className="px-2 mt-2 text-2xl font-bold text-white border border-white rounded hover:bg-slate-600">+</div>
         </Link>
 
       </div>
