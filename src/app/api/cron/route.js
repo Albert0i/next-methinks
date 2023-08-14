6e96e66e62aa646fab6e789f7ff69267eadc5a6d
Alberto Iong <albert0i@hotmail.com>
2023-08-14 13:20:25 +0800

    re-add cron job

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -9,4 +9,24 @@ export async function GET() {
 /*
    NextJS | Cron Jobs
    https://vercel.com/docs/cron-jobs
+
+  vercel.json
+  {
+    "crons": [
+      {
+        "path": "/api/cron",
+        "schedule": "0 * * * *"
+      }
+    ]
+  }
+
+   ┌───────────── minute (0 - 59)
+   │ ┌───────────── hour (0 - 23)
+   │ │ ┌───────────── day of the month (1 - 31)
+   │ │ │ ┌───────────── month (1 - 12)
+   │ │ │ │ ┌───────────── day of the week (0 - 6) (0 is Sunday, 6 is Saturday)
+   │ │ │ │ │
+   │ │ │ │ │
+   │ │ │ │ │
+   * * * * *
 */
