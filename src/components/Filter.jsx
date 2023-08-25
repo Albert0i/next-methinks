ccb998ef590e574e3c8035df4b4cd713f179974b
Alberto Iong <albert0i@hotmail.com>
2023-08-25 08:27:53 +0800

    lengthen search value input box

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -37,7 +37,7 @@ const Filter = () => {
     <div className='flex flex-row justify-between'>
       <div className='pl-2 text-sm font-bold align-bottom'>{ count }</div>
       <div className='flex flex-row justify-end px-2 mb-1'>
-          <input className='w-1/4 px-1 border border-black rounded' placeholder='full text value' autoFocus type='text' value={filterValue} onChange={ e => setFilterValue(e.target.value)}></input>
+          <input className='w-1/3 px-1 border border-black rounded' placeholder='text to search' autoFocus type='text' value={filterValue} onChange={ e => setFilterValue(e.target.value)}></input>
           
           <button className='px-1 ml-2 font-bold text-white bg-green-600 rounded hover:bg-green-400 disabled:bg-gray-400 disabled:cursor-not-allowed' onClick={ handleClick } disabled={disabled}>Filter</button>
 
